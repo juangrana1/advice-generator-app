61e26c7726aa7069081d6692bfb23940848dcdf2
jgm011 <jgm01@proton.me>
2023-12-01 14:14:36 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,52 @@
 import Divider from "./images/pattern-divider-desktop.svg";
 import Dice from "./images/icon-dice.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useTransition } from "react";
+
+interface Advice {
+  id: number;
+  advice: string;
+}
 
 function App() {
   const randomNumber = Math.floor(Math.random() * 224);
-  const [fetchedData, setFetchedData] = useState<{
-    id: number;
-    advice: string;
-  }>({ id: 0, advice: "" });
+  const [fetchedData, setFetchedData] = useState<Advice>({
+    id: 0,
+    advice: "",
+  });
   const [randomId, setRandomId] = useState<number>(randomNumber);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    try {
-      const fetchData = async () => {
-        const response = await fetch(
-          `https://api.adviceslip.com/advice/${randomId}`
-        );
-        const data = await response.json();
-        setFetchedData(data.slip);
-        setIsLoading(false);
-      };
-      fetchData();
-    } catch (error) {
-      console.log(error);
-    }
-  }), [randomId];
+    const fetchData = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://api.adviceslip.com/advice/${randomId}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data.slip);
+      setFetchedData(data.slip);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [randomId, setFetchedData, setIsLoading]);
+
+  const [isPending, startTransition] = useTransition();
 
   function randomizeAdvice() {
-    setIsLoading(true);
-    setRandomId(randomNumber);
+    startTransition(() => {
+      setRandomId(randomNumber);
+    });
   }
 
   return (
     <div className="background">
       <div className="container">
-        {!isLoading ? (
+        {!fetchedData ? (
+          <div className="loading">SOMETHING WENT WRONG... TRY AGAIN</div>
+        ) : !isLoading && !isPending ? (
           <>
             <div className="advice-number">{`ADVICE #${fetchedData.id}`}</div>
             <div className="advice">"{fetchedData.advice}"</div>
